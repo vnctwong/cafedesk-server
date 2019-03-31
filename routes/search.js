5d4185e6a3b4bfe74e948c8b904a18ad8627d198
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-31 15:46:02 -0700

    implement yelp api for searching

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const yelp = require('../api/yelp')
+
 module.exports = () => {
   router.get('/:keyword', (req, res) => {
-    res.send(`Search for: ${req.params.keyword}`);
+    const temp = yelp.search(req.params.keyword);
+    // res.send(`Search for: ${req.params.keyword}`);
+    res.send(`${temp}`);
   });
 
   return router;
