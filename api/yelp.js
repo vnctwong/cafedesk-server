848dffadbf46d9affdbeab1b47b80882c0f38296
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-31 16:00:15 -0700

    fix search in yelp

--- api/yelp.js
+++ api/yelp.js
@@ -2,17 +2,11 @@ const axios = require('axios');
 require('dotenv').config();
 
 function search(querry, longitude = -123.1207, latitude = 49.2827) {
-  axios.get(`https://api.yelp.com/v3/businesses/search?term=${querry}&latitude=${latitude}&longitude=${longitude}`, {
-      headers: {
-        Authorization: `Bearer ${process.env.YELP_KEY}`
-      },
-    })
-    .then(response => {
-      return response.data;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  return axios.get(`https://api.yelp.com/v3/businesses/search?term=${querry}&latitude=${latitude}&longitude=${longitude}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.YELP_KEY}`
+    },
+  });
 }
 
 module.exports = {
--- routes/search.js
+++ routes/search.js
@@ -5,9 +5,14 @@ const yelp = require('../api/yelp')
 
 module.exports = () => {
   router.get('/:keyword', (req, res) => {
-    const temp = yelp.search(req.params.keyword);
+    yelp.search(req.params.keyword)
+      .then(response => {
+        res.send(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     // res.send(`Search for: ${req.params.keyword}`);
-    res.send(`${temp}`);
   });
 
   return router;
