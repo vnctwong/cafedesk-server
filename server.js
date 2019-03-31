a9592e40f5a1c2e02b02bd00afe711be78a0aaf6
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-31 14:05:13 -0700

    populate with test routes and response

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const app = express();
 
 // require routes
 const usersRoutes = require('./routes/users');
+const tagsRoutes = require('./routes/users');
+const businessesRoutes = require('./routes/businesses');
+const searchRoutes = require('./routes/search');
+const recommendationsRoutes = require('./routes/recommendations');
 
 // mount routes
 app.use('/users', usersRoutes());
+app.use('/tags', tagsRoutes());
+app.use('/businesses', businessesRoutes());
+app.use('/search', searchRoutes());
+app.use('/recommendations', recommendationsRoutes());
 
 app.listen(PORT, () => {
   console.log('Example app listening on port ' + PORT);
