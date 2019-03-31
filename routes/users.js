9aee56d7cd339df0cffdfcd23661259c6615a9a8
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-31 14:44:35 -0700

    update with better test responses

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,23 @@ module.exports = () => {
   router.get('/', (req, res) => {
     res.send('boop');
   });
+  router.get('/:user_id', (req, res) => {
+    res.send(`User ${req.params.user_id}`);
+  });
+  router.get('/:user_id/favourites', (req, res) => {
+    res.send(`User ${req.params.user_id}'s favourites`);
+  });
+  router.get('/:user_id/views', (req, res) => {
+    res.send(`User ${req.params.user_id}'s view history`);
+  });
 
   // create new category
-  router.post('/new', (req, res) => {});
+  router.post('/:user_id/favourites/:favourite_id', (req, res) => {
+    res.send(`User ${req.params.user_id}'s favourite with id ${req.params.favourite_id}`);
+  });
+  router.post('/:user_id/favourites/:view_id', (req, res) => {
+    res.send(`User ${req.params.user_id}'s view with id ${req.params.view_id}`);
+  });
 
   return router;
 }
