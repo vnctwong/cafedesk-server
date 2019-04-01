2c4051cf2a8acbc0261069a061bccf7c21407d40
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-31 19:30:48 -0700

    fix spelling, querry -> query

--- api/yelp.js
+++ api/yelp.js
@@ -2,16 +2,16 @@ const axios = require('axios');
 require('dotenv').config();
 
 // build and send search request to yelp api, returns promise
-function search(querry, longitude = -123.1207, latitude = 49.2827) {
-  return axios.get(`https://api.yelp.com/v3/businesses/search?term=${querry}&latitude=${latitude}&longitude=${longitude}`, {
+function search(query, longitude = -123.1207, latitude = 49.2827) {
+  return axios.get(`https://api.yelp.com/v3/businesses/search?term=${query}&latitude=${latitude}&longitude=${longitude}`, {
     headers: {
       Authorization: `Bearer ${process.env.YELP_KEY}`
     },
   });
 }
 
-function getBusiness(querry) {
-  return axios.get(`https://api.yelp.com/v3/businesses/${querry}`, {
+function getBusiness(query) {
+  return axios.get(`https://api.yelp.com/v3/businesses/${query}`, {
     headers: {
       Authorization: `Bearer ${process.env.YELP_KEY}`
     },
