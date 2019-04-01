a69c1da1ae29ec0f533428c7c8235e8a516155a5
VWMAC <vwmac@VWMACs-MacBook-Air.local>
2019-04-01 16:06:09 -0700

    add sample user seeds to Users table

---
+++ seeders/20190401223347-sample-users.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      name: 'User1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      name: 'User2',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      name: 'User3',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      name: 'User4',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      name: 'User5',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, {
+      name: 'User6',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('Users', null, {});
+
+  }
+};
