ce1e82654ce234c53ce2343d5e57a1bf748c762b
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-31 20:03:10 -0700

    allow all origins to access server

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const ENV = process.env.ENV || 'development';
 const express = require('express');
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // require routes
 const usersRoutes = require('./routes/users');
 const tagsRoutes = require('./routes/tags');
