3b4936c3c1adc5597dff716ab17cb736b81671c1
Yves Desjardins <YvesDesjardins@gmail.com>
2019-04-02 15:10:08 -0700

    add cookie parser

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const PORT = process.env.PORT || 8080;
-const ENV = process.env.ENV || 'development';
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const app = express();
 
+app.use(cookieParser());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
