5b44c02e7648358274e13f091bca495372aef407
Yves Desjardins <YvesDesjardins@gmail.com>
2019-04-05 12:12:14 -0700

    remove redundant assosication in viewed

--- models/user_viewed_business.js
+++ models/user_viewed_business.js
@@ -2,10 +2,6 @@ module.exports = (sequelize, DataTypes) => {
   const User_viewed_business = sequelize.define('User_viewed_business', {
     viewed: DataTypes.BOOLEAN
   }, {});
-  User_viewed_business.associate = (models) => {
-    // associations can be defined here
-    User_viewed_business.belongsTo(models.Business);
-    User_viewed_business.belongsTo(models.User);
-  };
+
   return User_viewed_business;
 };
