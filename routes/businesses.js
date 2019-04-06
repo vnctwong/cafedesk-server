30ff7315076b63234c9ea87bbd441a508b1efcfc
Yves Desjardins <YvesDesjardins@gmail.com>
2019-04-05 18:22:58 -0700

    update routes

--- routes/businesses.js
+++ routes/businesses.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const yelp = require('../api/yelp');
+const db = require('../models');
 
 const router = express.Router();
 
 module.exports = () => {
   router.get('/', (req, res) => {
-    res.send('All businesses');
+    db.Business.findAll()
+      .then((businesses) => {
+        res.status(200).send(businesses);
+      });
   });
+  router.post('/', (req, res) => {
+    db.Business
+      .create({
+        name: req.params.business.name,
+        img_url: req.params.business.image_url,
+        address: req.params.business.location.display_address.join(' '),
+        longitude: req.params.business.coordinates.longitude,
+        latitude: req.params.business.coordinates.latitude,
+        yelp_id: req.params.business.id,
+      })
+      .then(() => {
+        res.status(200).send('Created business');
+      });
+  });
+
   router.get('/:business_id', (req, res) => {
     yelp.getBusiness(req.params.business_id)
-      .then((response) => {
-        res.status(200).send(response.data);
+      .then((yelpResult) => {
+        db.Business.findOne({
+            where: {
+              yelp_id: yelpResult.data.id,
+            },
+          })
+          .then((specificBusiness) => {
+            if (specificBusiness) {
+              // hard coded for now, to pull additional info from specificBusiness
+              yelpResult.data.ourTags = ['quiet', 'laptop friendly'];
+            }
+            res.status(200).send(yelpResult.data);
+          });
       })
       .catch((error) => {
         res.status(500).send(error);
