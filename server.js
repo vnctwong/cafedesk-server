936802fe04c2131745625114048f376202cddab1
Yves Desjardins <YvesDesjardins@gmail.com>
2019-04-09 16:37:36 -0700

    remote cookie parser

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const PORT = process.env.PORT || 8080;
 const express = require('express');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(cookieParser());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
